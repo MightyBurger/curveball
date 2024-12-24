be426b961360d847c3eede992e51b629eecc7895
Jordan Johnson <52584279+MightyBurger@users.noreply.github.com>
2024-12-23 22:26:56 -0700

    first draft of lump code

--- curveball-lib/src/lump.rs
+++ curveball-lib/src/lump.rs
@@ -1,3 +1,5 @@
+const TEX_DEFAULT: &str = "mtrl/invisible";
+
 #[derive(Debug, Default, Clone, Copy, PartialOrd, PartialEq)]
 pub struct Point {
     pub x: f64,
@@ -27,16 +29,22 @@ impl From<(f64, f64, f64)> for Point {
 
 // Not Copy to keep adding a texture in the realm of possibilities
 #[derive(Debug, Default, Clone, PartialOrd, PartialEq)]
-pub struct Side(pub [Point; 3]);
+pub struct Face {
+    points: [Point; 3],
+    texture: String,
+}
 
 use chull::ConvexHullWrapper;
 
 #[derive(Debug, Clone)]
-pub struct Lump(pub ConvexHullWrapper<f64>);
+pub struct Lump {
+    points: Vec<Point>,
+    faces: Vec<([usize; 3], String)>, // the array contains the indices into the vertices vec
+}
 
 impl Lump {
-    pub fn try_from_points(
-        points: impl IntoIterator<Item = Point>,
+    pub fn try_from_points<'a>(
+        points: impl IntoIterator<Item = &'a Point>,
         max_iter: Option<usize>,
     ) -> Result<Self, chull::convex::ErrorKind> {
         let points: Vec<Vec<f64>> = points
@@ -45,52 +53,153 @@ impl Lump {
             .collect();
 
         let hull = ConvexHullWrapper::try_new(&points, max_iter)?;
-        Ok(Self(hull))
+
+        Ok(hull.into())
     }
 
-    pub fn sides(&self) -> impl Iterator<Item = Side> {
-        let (vertices, indices) = self.0.vertices_indices();
-
-        use itertools::Itertools;
-        indices.into_iter().tuples().map(move |(idx1, idx2, idx3)| {
-            Side([
-                Point {
-                    x: vertices[idx1][0],
-                    y: vertices[idx1][1],
-                    z: vertices[idx1][2],
-                },
-                Point {
-                    x: vertices[idx2][0],
-                    y: vertices[idx2][1],
-                    z: vertices[idx2][2],
-                },
-                Point {
-                    x: vertices[idx3][0],
-                    y: vertices[idx3][1],
-                    z: vertices[idx3][2],
-                },
-            ])
+    pub fn to_faces(&self) -> impl Iterator<Item = Face> + use<'_> {
+        self.faces.iter().map(|([idx0, idx1, idx2], tex)| Face {
+            points: [self.points[*idx0], self.points[*idx1], self.points[*idx2]],
+            texture: tex.clone(),
         })
     }
 
-    pub fn points(&self) -> impl Iterator<Item = Point> {
-        self.sides().flat_map(|side| side.0.into_iter())
+    pub fn points(&self) -> impl Iterator<Item = &Point> + use<'_> {
+        self.points.iter()
     }
 }
 
 impl From<ConvexHullWrapper<f64>> for Lump {
-    fn from(value: ConvexHullWrapper<f64>) -> Self {
-        Self(value)
+    fn from(hull: ConvexHullWrapper<f64>) -> Self {
+        let (points, face_indices) = hull.vertices_indices();
+
+        let points = points
+            .into_iter()
+            .map(|vertex| Point {
+                x: *vertex
+                    .get(0)
+                    .expect("vertices expected to have three components"),
+                y: *vertex
+                    .get(1)
+                    .expect("vertices expected to have three components"),
+                z: *vertex
+                    .get(2)
+                    .expect("vertices expected to have three components"),
+            })
+            .collect();
+
+        // Just a check.
+        assert_eq!(face_indices.len() % 3, 0);
+
+        // When ArrayChunks is stabalized, switch to using that over tuples() and eliminate
+        // the itertools dependancy.
+        use itertools::Itertools;
+        let faces: Vec<([usize; 3], String)> = face_indices
+            .into_iter()
+            .tuples()
+            .map(|(idx1, idx2, idx3)| ([idx1, idx2, idx3], String::from(TEX_DEFAULT)))
+            .collect();
+
+        // TODO: Eliminate equivalent faces. It appears the library I use will split each
+        // lump into triangles, producing excessive faces, though the geometry will
+        // be the same.
+        // This is an important step, as currently, lumps will have twice the number
+        // of faces as required.
+        // Test every face against every other face, and delete when two faces match.
+
+        Self { points, faces }
     }
 }
 
-impl Into<ConvexHullWrapper<f64>> for Lump {
-    fn into(self) -> ConvexHullWrapper<f64> {
-        self.0
-    }
-}
+// -----------------------------------
+//             Unit Tests
+// -----------------------------------
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const EPSILON: f64 = 0.000000001;
+    fn almost_equals(a: &Point, b: &Point) -> bool {
+        (a.x - b.x).abs() < EPSILON && (a.y - b.y).abs() < EPSILON && (a.z - b.z).abs() < EPSILON
+    }
+
+    #[test]
+    fn test_lump_cube() {
+        let points = vec![
+            Point::from([0.0, 0.0, 0.0]),
+            Point::from([0.0, 0.0, 1.0]),
+            Point::from([0.0, 1.0, 0.0]),
+            Point::from([0.0, 1.0, 1.0]),
+            Point::from([1.0, 0.0, 0.0]),
+            Point::from([1.0, 0.0, 1.0]),
+            Point::from([1.0, 1.0, 0.0]),
+            Point::from([1.0, 1.0, 1.0]),
+            Point::from([0.5, 0.5, 0.5]),
+        ];
+
+        let lump = Lump::try_from_points(&points, Some(1000)).unwrap();
+
+        let extracted_points: Vec<&Point> = lump.points().collect();
+        let extracted_faces: Vec<Face> = lump.to_faces().collect();
+
+        assert_eq!(extracted_points.len(), 8);
+        assert_eq!(extracted_faces.len(), 12); // TODO: Change to 6 when the above TODO is resolved.
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([0.0, 0.0, 0.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([0.0, 0.0, 1.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([0.0, 1.0, 0.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([0.0, 1.0, 1.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([1.0, 0.0, 0.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([1.0, 0.0, 1.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([1.0, 1.0, 0.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([1.0, 1.0, 1.0]))));
+    }
+
+    #[test]
+    fn test_lump_pyramid() {
+        let points = vec![
+            Point::from([0.0, 0.0, 0.0]),
+            Point::from([0.0, 0.0, 1.0]),
+            Point::from([0.0, 1.0, 0.0]),
+            Point::from([1.0, 0.0, 0.0]),
+            Point::from([0.3, 0.3, 0.3]),
+        ];
+
+        let lump = Lump::try_from_points(&points, Some(1000)).unwrap();
+
+        let extracted_points: Vec<&Point> = lump.points().collect();
+        let extracted_faces: Vec<Face> = lump.to_faces().collect();
+
+        assert_eq!(extracted_points.len(), 4);
+        assert_eq!(extracted_faces.len(), 4);
+
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([0.0, 0.0, 0.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([0.0, 0.0, 1.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([0.0, 1.0, 0.0]))));
+        assert!(extracted_points
+            .iter()
+            .any(|point| almost_equals(point, &Point::from([1.0, 0.0, 0.0]))));
+    }
 }
